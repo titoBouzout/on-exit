b660474faa1775ef9d9dd028d29d8315a44a52fe
Tito <tito.bouzout@gmail.com>
2022-05-19 05:58:15 -0300

    update

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 let exiting = false
 
-function onExit(who) {
+function onExit() {
 	if (!exiting) {
 		exiting = true
-		if (who == 'SIGHUP' || who == 'SIGBREAK' || who == 'SHUTDOWN') {
-			process.exit(0)
-		} else {
-			return Promise.all(promises).then(function () {
-				process.exit(0)
-			})
-		}
+		for (let cb of callbacks) cb()
 	}
 }
 
-process.on('SIGTERM', () => onExit('SIGTERM'))
-process.on('SIGINT', () => onExit('SIGINT'))
-process.on('SIGUSR2', () => onExit('SIGUSR2'))
-process.on('SIGBREAK', () => onExit('SIGBREAK'))
-process.on('SIGHUP', () => onExit('SIGHUP'))
+let callbacks = []
+module.exports = function (cb) {
+	callbacks.push(cb)
+}
 
-//process.on('SIGKILL', () => onExit('SIGKILL')) THIS MAKE IT CRASH ON LINUX! eehh
-//process.on('exit', () => onExit('exit'))
-
-process.on('message', m => {
-	if (m && (m == 'RESTART' || m == 'SHUTDOWN')) {
-		onExit(m)
-	}
-})
-
-let promises = []
-module.exports = function (promise) {
-	promises.push(promise)
+for (let m of ['RESTART', 'SIGINT', 'SIGTERM', 'SIGUSR2', 'SIGHUP', 'exit', 'SIGBREAK']) {
+	process.on(m, onExit)
 }
